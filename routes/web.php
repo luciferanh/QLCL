80f666e51af675c3ac46709a152f2f697b6bae8e
phuong <nguyenphuonghoang14200@gmail.com>
2021-10-04 20:34:43 -0700

    navbar

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('admin/login', [AdminController::class,'index'])->name('login');
+Route::get('admin/login', [AdminController::class,'index']);
 Route::post('admin/store', [AdminController::class,'store']);
