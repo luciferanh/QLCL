c24d20aa47f59b7439d08fb9073a7782cf0b6f08
Trần Việt Anh <vietanh2392000@gmail.com>
2021-10-12 08:42:47 +0700

    change slide

--- resources/views/admin/themnv.blade.php
+++ resources/views/admin/themnv.blade.php
@@ -11,7 +11,7 @@
             <!-- jquery validation -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">{{$title}}i</h3>
+                <h3 class="card-title">{{$title}}</h3>
 
               </div>
               <!-- /.card-header -->
