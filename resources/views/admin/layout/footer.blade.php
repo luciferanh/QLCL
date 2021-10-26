5eeea262c6fa4e5f4154ada7d4175d9afefc6e8b
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-10-26 20:48:30 +0700

    change js

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,18 +1,18 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
 <!-- jQuery -->
-<script src="/template/plugins/jquery/jquery.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="/template/plugins/jquery-ui/jquery-ui.min.js"></script>
+
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 
 <!-- overlayScrollbars -->
-<script src="/template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="/template/dist/js/adminlte.js"></script>
+<script src="/js/jquery.overlayScrollbars.min.js"></script>
+
+<script src="/js/adminlte.js"></script>
 
 
 @yield('footer')
